63435254bcc48907eb4acbfb80720605f3e01fea
Kevin Hsu <hsukvn@gmail.com>
2018-06-03 22:51:40 +0800

    Algorithms[Medium] - Remove Nth Node From End of List - one pass

--- cpp/algorithms/remove-nth-node-from-end-of-list/main.cpp
+++ cpp/algorithms/remove-nth-node-from-end-of-list/main.cpp
@@ -20,15 +20,34 @@ public:
 		}
 
 		if (size == n) {
-            return head->next;
-        }
+			return head->next;
+		}
 
-		for (curr = head, int i = 0; i < (size - n - 1); i++, curr=curr->next);
+		int i = 0;
+		for (curr = head, i = 0; i < (size - n - 1); i++, curr=curr->next);
 
 		curr->next = curr->next->next;
 
 		return head;
-    }
+    };
+    ListNode* removeNthFromEnd2(ListNode* head, int n) {
+		ListNode *curr = head;
+		ListNode *p = head;
+
+		for (int i = 0; i < n; curr=curr->next, ++i);
+
+		if (curr == nullptr) {
+			return head->next;
+		}
+
+		while (curr->next != nullptr) {
+			p = p->next;
+			curr = curr->next;
+		}
+
+		p->next = p->next->next;
+		return head;
+    };
 };
 
 int main()
