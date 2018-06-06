14cd2076f4452fd1c1ccbbbe1474c4357734659b
Kevin Hsu <hsukvn@gmail.com>
2018-06-06 23:30:29 +0800

    Algorithms[Easy] - Two Sum (C++ better way)

--- cpp/algorithms/two-sum/main.cpp
+++ cpp/algorithms/two-sum/main.cpp
@@ -6,37 +6,15 @@ using namespace std;
 class Solution {
 	public:
 		vector<int> twoSum(vector<int>& nums, int target) {
-			vector<int> nums_bk(nums);
+			map<int, int> hash;
 
-			std::sort(nums.begin(), nums.end());
-
-			vector<int> sol;
-			int left = 0;
-			int right = nums.size() - 1;
-
-			while (left < right) {
-				if (nums[left] + nums[right] == target) {
-					for (int i = 0; i < nums_bk.size(); ++i) {
-						if (nums_bk[i] == nums[left]) {
-							sol.push_back(i);
-							break;
-						}
-					}
-					for (int j = nums_bk.size() - 1; j >= 0; --j) {
-						if (nums_bk[j] == nums[right]) {
-							sol.push_back(j);
-							break;
-						}
-					}
-					break;
-				} else if (nums[left] + nums[right] > target) {
-					right--;
+			for (int i = 0; i < nums.size(); ++i) {
+				if (hash.end() == hash.find(target - nums[i])) {
+					hash[nums[i]] = i;
 				} else {
-					left++;
+					return {hash[target-nums[i]], i};
 				}
 			}
-
-			return sol; 
 		}
 };
 
