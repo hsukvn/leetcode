1f42b2b4295ce28c830b46dcf3e81b3f89b682f2
kevinhsu <kevinhsu@synology.com>
2018-06-09 23:57:16 +0800

    Algorithms[Easy] - Maximum Subarray (refine)

--- cpp/algorithms/maximum-subarray/main.cpp
+++ cpp/algorithms/maximum-subarray/main.cpp
@@ -6,15 +6,12 @@ using namespace std;
 class Solution {
 	public:
 		int maxSubArray(vector<int>& nums) {
-			int v = nums[0];
-
-			vector<int> dp;
-			dp.push_back(nums[0]);
+			int v = nums[0], last = nums[0];
 
 			for (int i = 1; i < nums.size(); ++i) {
-				dp.push_back(max(dp[i-1] + nums[i], nums[i]));
-				if (dp[i] > v) {
-					v = dp[i];
+				last = max(last + nums[i], nums[i]);
+				if (last > v) {
+					v = last;
 				}
 			}
 
